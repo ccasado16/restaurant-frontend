6851b189c3958bfdc8026c9aaacac16bb17a4718
ccasado16 <carloscasadovargas@gmail.com>
2023-05-24 18:48:15 -0500

    Configure LoginForm template to perform user authentication

--- src/components/auth/components/loginForm/LoginForm.vue
+++ src/components/auth/components/loginForm/LoginForm.vue
@@ -1,26 +1,55 @@
-<script lang="ts"></script>
+<script lang="ts" setup>
+  import { ref } from "vue";
+  import { ILogin } from "../../models/IAuth";
+  import { VForm } from "vuetify/components";
+  import { useRouter } from "vue-router";
+  import authLogin from "../../actions/authLogin";
+
+  const authModel = ref({
+    username: "",
+    password: "",
+  } as ILogin);
+
+  const formRef = ref<InstanceType<typeof VForm> | null>(null);
+  const router = useRouter();
+
+  const login = async () => {
+    const response = await authLogin(authModel.value);
+
+    if (response) {
+      router.push("/");
+    }
+  };
+</script>
+
 <template>
   <div class="flex h-screen justify-center">
-    <v-card class="m-auto px-6 py-8 w-64">
-      <v-form>
-        <v-text-field class="mb-2" label="Username"></v-text-field>
+    <VCard class="m-auto px-6 py-8 w-64">
+      <VForm ref="formRef" :model="authModel">
+        <VTextField
+          class="mb-2"
+          label="Username"
+          v-model="authModel.username"
+        ></VTextField>
 
-        <v-text-field
+        <VTextField
           label="Password"
+          v-model="authModel.password"
           placeholder="Enter your password"
           type="password"
-        ></v-text-field>
+          @keydown.enter="login"
+        ></VTextField>
 
-        <v-btn
+        <VBtn
           block
           color="success"
           size="large"
-          type="submit"
           variant="elevated"
+          @click="login"
         >
           Sign In
-        </v-btn>
-      </v-form>
-    </v-card>
+        </VBtn>
+      </VForm>
+    </VCard>
   </div>
 </template>
