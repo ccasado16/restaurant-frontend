bd7cb81db4cf5ae24b5f05b1567fa43129d80523
ccasado16 <carloscasadovargas@gmail.com>
2023-05-25 12:32:06 -0500

    Redirect to login page if not logged in

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,19 @@ const routes = [
     path: "/login",
     name: "Login",
     component: () => import("../pages/LoginPage.vue"),
-  }
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (to.name !== "Login" && !token) next({ name: "Login" });
+  else next();
 });
 
 export default router;
